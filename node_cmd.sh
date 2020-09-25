bacb0c9e2ce4781a348933fff12eccae8ec858c2
Jan Vicha <john.vicha@seznam.cz>
2020-09-25 17:11:39 +0200

    fix - remove comments from scripts

--- node_cmd.sh
+++ node_cmd.sh
@@ -4,9 +4,9 @@ node_version=${args[0]}
 command=${args[@]:1}
 
 docker run \
-  -it \ # Stay keep interactive console.
-  --rm \ # Automatically remove the container when it exits.
-  --name local-nodejs-version-${node_version}-bin \ # Assign a name to the container.
+  -it \
+  --rm \
+  --name local-nodejs-version-${node_version}-bin \
   --user $(id -u):$(id -g) \
   -v $PWD:/usr/src/app:delegated \
   -w /usr/src/app \
